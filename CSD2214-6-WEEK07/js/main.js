97483173f6b623dda1b8d867da2b4aa74d27d793
Vasireddy Pavan <vasireddypavans131219@gmail.com>
2019-02-25 17:06:27 -0500

    HI

--- CSD2214-6-WEEK07/js/main.js
+++ CSD2214-6-WEEK07/js/main.js
@@ -1,1 +1,21 @@
-﻿// Your code here!
+﻿var bird = {
+    genius: "corvus",
+    species: "corvax",
+    commonname: "raven",
+    callType: "squawky",
+    quote: "Nevermore",
+    maxOffspring: 5,
+    noisy: true,
+    deadly: false
+};
+var bear = {
+    genius: "ursus",
+    species: "arctos",
+    commonname: "brown bear",
+    callType: "roar",
+    quote: "",
+    maxOffspring: 3,
+    noisy: true,
+    deadly: false
+};
+// Your code here!
